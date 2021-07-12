ffd4be55c7ebaf73e4288d0ef9614a59741bf31f
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-12 16:48:18 -0500

    New Qoute

--- 3-writeline/3-writeline.cs
+++ 3-writeline/3-writeline.cs
@@ -6,7 +6,7 @@ namespace holbertonschool_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(@""Programming is like building a multilingual puzzle");
+            Console.WriteLine(@"""Programming is like building a multilingual puzzle");
         }
     }
 }
