b3a907c59932848ce7c5cf08cdac9db2e919e987
HLozano12 <HLR8503@gmail.com>
2021-07-12 19:53:48 +0000

    OverThinking

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -2,7 +2,5 @@
 # initializes and builds a new C# project inside a folder titled 1-new_project
 
 
-mkdir 1-new_project
-cd 1-new_project
-dotnet new console 1-new_project
+dotnet new console -n 1-new_project
 dotnet build 1-new_project
