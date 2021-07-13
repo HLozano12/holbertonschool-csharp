9efe90b6aee0b925adad0cfdba3c90d4e3481ee2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 17:38:55 -0500

    Math.Abs

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            return Math.Abs(number % 10);
+        }
+    }
+}
