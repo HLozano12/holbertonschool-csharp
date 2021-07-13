fd406700506ca4798c13d621ff4d69bdac039dfc
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 15:31:53 -0500

    NewLine Hex

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hexa = 0; hexa <= 98; hexa++)
+				Console.WriteLine("{0} = 0x{0:x}", hexa);
+        }
+    }
+}
