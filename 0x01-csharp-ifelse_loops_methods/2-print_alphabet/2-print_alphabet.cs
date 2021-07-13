1d0de4311bbc06f6990d9e50782760f13a31e2ba
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 14:55:25 -0500

    Like Low Level

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char h = 'a'; h <= 'z'; h++)
+			Console.Write(h);
+        }
+    }
+}
