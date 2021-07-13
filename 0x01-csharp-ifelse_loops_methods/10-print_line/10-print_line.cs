8ca4a3e5b9c5a9ec2615b20e70a6a663bae2bd42
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 18:10:45 -0500

    Line Up

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            if (length <= 0)
+			{
+				Console.WriteLine();
+			}
+			else
+			{
+				for (int h = 0; h < length; h++)
+				{
+					Console.Write("_");
+				}
+				Console.WriteLine();
+			}
+        }
+    }
+}
