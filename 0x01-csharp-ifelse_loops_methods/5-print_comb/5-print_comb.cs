4e7fa0a34b9932d3dfb9379b783d09290505eab1
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 16:24:24 -0500

    CombTheDesert

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for  (int numb = 00; numb <= 00; numb++)
+			{
+				if (numb == 99)
+					Console.WriteLine($"{numb}");
+				else
+					Console.WriteLine($"{numb / 10}{numb % 10}, ");
+			}
+        }
+    }
+}
