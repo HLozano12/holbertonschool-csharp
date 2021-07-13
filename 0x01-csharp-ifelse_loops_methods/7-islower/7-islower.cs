3fdf2988d6fb3dd49da1e8558ec3af29deff0bd2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 16:59:12 -0500

    Like LowLevel

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (h >= 'a' && h <= 'z')
+			{
+				return (true);
+			}
+			else
+			{
+				return (false);
+			}
+        }
+    }
+}
