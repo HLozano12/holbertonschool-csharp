62c14ce507834fc3f32b9efa6b0f70dbb47e2a57
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 16:50:18 -0500

    Typo

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -11,7 +11,7 @@ namespace _6_print_comb2
 				int n1 = numb / 10;
 				int n2 = numb % 10;
 				if (numb != 89 && n1 < n2)
-					Console.Write($"{num:D2}, ");
+					Console.Write($"{numb:D2}, ");
 				else if (numb == 89)
 					Console.WriteLine($"{numb:D2}");
 			}
