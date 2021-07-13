131896a2a6cc77343a713db0248599b795fb4515
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 16:46:32 -0500

    2digt combo

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numb = 00; numb <= 90; numb++)
+			{
+				int n1 = numb / 10;
+				int n2 = numb % 10;
+				if (numb != 89 && n1 < n2)
+					Console.Write($"{num:D2}, ");
+				else if (numb == 89)
+					Console.WriteLine($"{numb:D2}");
+			}
+        }
+    }
+}
