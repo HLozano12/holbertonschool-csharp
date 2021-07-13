aeecae9ba5f64d8961e3c343b7600d4ea90457d2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 14:17:55 -0500

    Alignment

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
-		int h = number % 10;
-		string statement;
+        int h = number % 10;
+        string statement;
 
-		if (h > 5)
-		{
-			statement = "and is greater than 5";
-		}
-		else if (statement == 0)
-		{
-			statement = "and is 0";
-		}
-		else
-		{
-			statement = "and is less than 6 and not 0";
-		}
-		Console.WriteLine("The last digit of {0} is {1}{2}", number, h, statement);
+        if (lastDigit > 5)
+        {
+            statement = " and is greater than 5";
+        }
+        else if (lastDigit == 0)
+        {
+            statement = " and is 0";
+        }
+        else
+        {
+            statement = " and is less than 6 and not 0";
+
+        }
+        Console.WriteLine("The last digit of {0} is {1}{2}", number, h, statement);
     }
 }
