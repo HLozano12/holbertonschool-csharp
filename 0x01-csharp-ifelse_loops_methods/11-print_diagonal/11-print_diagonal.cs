0fe1a3fcfa9bf7d56eb8d6c5fe77e7902b9975c2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-13 18:28:02 -0500

    Diagonalley

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            for (int h = 0; h < length; h++)
+			{
+				for (int a = 0; a < h; a++)
+					Console.Write(" ");
+				Console.WriteLine("\\");
+			}
+			Console.Write("\n");
+        }
+    }
+}
