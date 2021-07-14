ef0cde48920bead7594c5567960b55ca94c7752b
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 14:17:46 -0500

    New Array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+	// Print Array of Given Size
+	// blank if size is zero, "Cannot be Neg" and return Null if >0
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size == 0)
+			{
+				Console.WriteLine();
+			}
+			if (size < 0)
+			{
+				Console.WriteLine("Size cannot be negative");
+			}
+			int[] Array = new int[size];
+
+			for (int h = 0; h < size; h++)
+			{
+				Array[h] = h;
+				Console.Write(Array[h]);
+				if (h == size - 1)
+				{
+					Console.WriteLine();
+				}
+				else
+				{
+					Console.Write(" ");
+				}
+			}
+			return (Array);	
+        }
+    }
+}
