ab5bd2899fb7a9332987fec8d08e5cb437783d61
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 16:13:00 -0500

    Replacement

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+    class Array
+	// write method that replaces an element of an array at a given index.
+    {
+        public static int[] ReplaceElement(int[] array, int index, int n)
+		// return an update Array
+		// If index is out of range, print Index out of range and 
+		// return the unchanged array
+        {
+			if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+        }
+        else
+        {
+            array[index] = n;
+        }
+        return (array);
+        }
+    }
