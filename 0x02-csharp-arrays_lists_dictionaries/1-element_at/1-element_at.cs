299169865e1e2d22c34440d08d69b4110466ca23
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 15:44:33 -0500

    OutOfRange

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+		// Retrieve an element from an array
+		// If index is out of range, print Index out of range and return -1
+        {
+            if (array.Length <= index || index < 0)
+			{
+				Console.WriteLine("Index out of range");
+				return (-1);
+			}
+			return (array[index]);
+        }
+    }
