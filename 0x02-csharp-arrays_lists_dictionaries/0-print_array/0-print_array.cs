065c5b355914978d2ba4211d85f1081c027d460b
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 14:52:58 -0500

    RmvNamespace

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-namespace _0_print_array
-{
     class Array
 	// Print Array of Given Size
 	// blank if size is zero, "Cannot be Neg" and return Null if >0
@@ -9,29 +7,30 @@ namespace _0_print_array
         public static int[] CreatePrint(int size)
         {
             if (size == 0)
-			{
-				Console.WriteLine();
-			}
-			if (size < 0)
-			{
-				Console.WriteLine("Size cannot be negative");
-			}
-			int[] Array = new int[size];
-
+        	{
+            	Console.WriteLine();
+        	}
+        	if (size < 0)
+        	{
+            	Console.WriteLine("Size cannot be negative");
+            	return (null);
+        	}
+        	int[] NewArray = new int[size];
+        
 			for (int h = 0; h < size; h++)
-			{
-				Array[h] = h;
-				Console.Write(Array[h]);
+        	{
+            	NewArray[h] = h;
+            	Console.Write(NewArray[h]);
+            	
 				if (h == size - 1)
-				{
-					Console.WriteLine();
-				}
-				else
-				{
-					Console.Write(" ");
-				}
-			}
-			return (Array);	
+            	{
+                	Console.WriteLine();
+            	}
+            	else
+            	{
+                	Console.Write(" ");
+            	}
+        	}
+        	return (NewArray);
         }
     }
-}
