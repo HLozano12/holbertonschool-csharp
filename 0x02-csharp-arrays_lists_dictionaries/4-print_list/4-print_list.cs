2d59d41d2693034f39c3e645754d74304c9d6913
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 17:38:20 -0500

    Foreach

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        // Print a list of ints of given size
+		{
+            // int should start w/ 0 and ++ until given size
+			// return new list
+			// if 0 < size print Size cannot be neg, return NULL
+			if (size < 0)
+        	{
+            	Console.WriteLine("Size cannot be negative");
+            	return null;
+        	}
+
+        	List<int> list = new List<int>();
+        	for (int h = 0; h < size; h++)
+        	{
+            	list.Add(i);
+        	}
+        	foreach (int h in list)
+        	{
+            	Console.Write(h);
+            	if (h != size - 1)
+            	{
+                	Console.Write(" ");
+            	}
+        	}
+        	Console.WriteLine();
+        	return list;
+        }
+    }
