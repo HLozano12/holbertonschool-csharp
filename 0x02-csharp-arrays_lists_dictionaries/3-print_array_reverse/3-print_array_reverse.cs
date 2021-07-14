4644799143e6d12de74a28b8b072bf1bfb9a0382
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 16:42:57 -0500

    FlipIt&reverseIt

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+		// print all integer of an array, in revrse
+    		{
+        		if (array != null)
+        		{
+            		for (int h = array.Length - 1; h >= 0; h--)
+					// Reverse
+            		{
+                		Console.Write(array[h]);
+                		if (h != 0)
+                		{
+                    		Console.Write(" ");
+                		}
+            		}
+        		}    
+        		Console.WriteLine();
+        	}
+    }
