de2fab6e653d899cfdbc9327a0d827cc255d934c
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 18:14:58 -0500

    To TheMAx

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+	// Find the biggest integer of a list 
+    {
+        public static int MaxInteger(List<int> myList)
+		// Return largest element in a given list
+		// if list empty, print "List is empty" and return -1
+		// Not allowed to use .Max()
+        {
+            if (myList.Count == 0 || myList == null)
+        {
+            Console.WriteLine("List is empty");
+            return (-1);
+        }
+        // .Sort() Method is used to sort the elements in the List<T>
+		// GeeksforGeeks Ref
+        myList.Sort();
+        return (myList[myList.Count -1]);
+        }
+    }
