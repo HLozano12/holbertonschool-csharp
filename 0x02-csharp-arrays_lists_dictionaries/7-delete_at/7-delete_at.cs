5cd477f0cbc525afa261f248d7412a0c3cc539d4
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 19:51:14 -0500

    Range

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+	// Write a method that deletes the item 
+	// at a specific position in a list
+    {
+        public static List<int> DeleteAt(List<int> myList, int index)
+		// return Updated List
+		// If index is out of range, print Index is out of range
+		// and return the unchanged list
+		// Cannot use RemoveAt
+        {
+            if (index < 0 || index >= myList.Count)
+			// If negative or less thean
+			{
+				Console.WriteLine("Index is out of range");
+			}
+			else
+			{
+				myList.RemoveRange(index, 1);
+				// removes a range of elements from ArrayList
+			}
+			return (myList);
+        }
+    }
