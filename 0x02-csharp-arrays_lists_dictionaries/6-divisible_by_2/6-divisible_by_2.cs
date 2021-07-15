0daf9beb5ef9541d693159971dc0769dfe88b851
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-14 19:21:32 -0500

    Even?

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+	// Find all Multiples of 2 in a list
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+		// Return list with True or False depending on requirements
+		// Both list should be the same size
+        {
+			List<bool> TheNewList = new List<bool>();
+            foreach (int h in myList)
+			// Looping on each element
+        	{
+            	if ((h % 2) == 0)
+				// if diviable by 2 
+            	{
+                	TheNewList.Add(true);
+            	}
+            	else
+				// if not
+            	{
+                	TheNewList.Add(false);
+            	}
+        	}
+        	return TheNewList;
+        }
+    }
