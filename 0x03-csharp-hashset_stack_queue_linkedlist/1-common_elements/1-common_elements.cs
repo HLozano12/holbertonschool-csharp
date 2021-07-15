a7d3d62e9ef9ae0f30329ac172afb222e887928a
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-15 15:41:44 -0500

    FindAll Learned

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+	// Return sorted list of common elements in two list
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        // Not able to use linq or sortedlist
+		{
+            List<int> NewList = list1.FindAll(element => list2.Contains(element));
+			return (NewList);
+        }
+    }
