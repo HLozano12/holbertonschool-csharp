cb67f5de8b2acea3d4932a5052e3d9aee4d6e647
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-15 16:21:39 -0500

    BeDiffernt

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            HashSet<int> Set1 = new HashSet<int>(list1);
+			HashSet<int> Set2 = new HashSet<int>(list2);
+        	HashSet<int> NewHash = new HashSet<int>(Set1);
+
+			NewHash.SymmetricExceptWith(Set2);
+			List<int> NewList = new List<int>(NewHash);
+			NewList.Sort();
+			return (NewList);
+        }
+    }
