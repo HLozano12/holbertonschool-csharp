486ddc9db7f874a65261a57cff2686bbad2c74dd
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-15 15:24:51 -0500

    AddItUp

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int sum = 0;
+			HashSet<int> add = new HashSet<int>(myList);
+
+			foreach (int digit in add)
+			{
+				sum += digit;
+			}
+			return (sum);
+        }
+    }
