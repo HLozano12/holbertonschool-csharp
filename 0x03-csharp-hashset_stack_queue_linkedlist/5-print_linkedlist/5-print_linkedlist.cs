95d3e3382c1de256e409d2060af6128b2148723d
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 15:02:54 -0500

    LList

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        for (int h = 0; h < size; h++)
+        {
+            myLList.AddLast(h);
+            Console.WriteLine(h);
+        }
+
+        return myLList;
+    }
+}
