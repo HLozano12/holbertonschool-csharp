de21bc57a655a63c0e16daa2672a0a2ead5cba47
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 15:13:01 -0500

    LLength

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+	// Our Prototype
+	// return elements in a linked list
+    {
+        int count = 0;
+
+        foreach (int item in myLList)
+		// loop
+        {
+            count += 1;
+        }
+        return (count);
+    }
+}
