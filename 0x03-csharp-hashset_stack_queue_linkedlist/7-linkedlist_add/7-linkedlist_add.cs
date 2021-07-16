da107e1f90d36401ed2c2232573b42256d2acfb0
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 15:23:25 -0500

    ToTheFront

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+// link list
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+	// add a node to beginning of link list
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        myLList.AddFirst(newNode);
+		// adds a node at the start of LL
+        return (newNode);
+    }
+}
