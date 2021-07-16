6b997497975b39baa3696417a4591c96a6a8dfe3
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 13:58:54 -0500

    aQueue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+    class MyQueue
+    {
+        public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+        {
+            Console.WriteLine("Number of items: {0}", aQueue.Count);
+			// Print number in aQueue
+
+			if (aQueue.Count == 0 || aQueue == null)
+				// if queue is empty
+			{
+				Console.WriteLine("Queue is empty");
+			}
+			else
+        	{
+            	Console.WriteLine("First item: {0}", aQueue.Peek());
+        	}
+
+        	aQueue.Enqueue(newItem);
+
+        	Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        	if (aQueue.Contains(search))
+        	{
+            	while (aQueue.Count > 0)
+            	{
+                	string value = aQueue.Dequeue();
+                	if (value == search)
+                	{
+                    	break;
+                	}
+            	}
+        	}
+
+        	return aQueue;
+        }
+    }
