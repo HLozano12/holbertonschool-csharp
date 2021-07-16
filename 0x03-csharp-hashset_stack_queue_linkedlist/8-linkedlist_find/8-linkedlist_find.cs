a3257dd51b1c29065966f11f6f37cdd77e0e7578
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 15:40:25 -0500

    AddVal

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+// link list
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+	// return indx pos of first occr of value gvn or -1
+	// if val not in list
+    {
+        int index = -1;
+		// if no given value
+
+        foreach (int Mynode in myLList)
+        {
+            index += 1;
+            if (Mynode == value)
+            {
+                break;
+            }
+            else if (index == myLList.Count - 1)
+            {
+                return -1;
+            }
+        }
+        return index;
+    }
+}
