0e80d5ca2b740e1c51494bd02430acda5d69fcc7
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 23:28:15 -0500

    NumbKeysDict

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+// change class per task
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+	// return the number of keys in a dict
+    {
+        int h = 0;
+        foreach (KeyValuePair<string, string> pairs in myDict)
+        {
+            h++;
+        }
+        return h;
+    }
+}
