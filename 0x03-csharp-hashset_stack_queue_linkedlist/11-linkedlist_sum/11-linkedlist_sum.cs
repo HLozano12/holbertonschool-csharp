e9b895af15b5fe0abf641edec18fa731fc044d97
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 20:22:28 -0500

    Add It UP

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+// LinkList
+{
+    public static int Sum(LinkedList<int> myLList)
+	// Rtrn the sum of all data in LnkLst
+    {
+        int sum = 0;
+		// sum set to zero
+
+        foreach (int indx in myLList)
+		// Loop through items in LL
+        {
+            sum += indx;
+        }
+        return (sum);
+		// rtrn data of all indx of LL added up
+     }
+}
