c3fa862e2399f59364c0f894275ab974b8cf8922
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 20:00:45 -0500

    Remv and Know

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+//Link List
+{
+    public static int Pop(LinkedList<int> myLList)
+	// Delete the head node of LL and rtrn node data
+    {
+        if (myLList.Count == 0)
+		// if list is empty return zero
+        {
+            return (0);
+        }
+        int nValue = myLList.First.Value;
+        myLList.RemoveFirst();
+		// func removes node at start
+        return (nValue);
+		// return value of node
+    }
+}
