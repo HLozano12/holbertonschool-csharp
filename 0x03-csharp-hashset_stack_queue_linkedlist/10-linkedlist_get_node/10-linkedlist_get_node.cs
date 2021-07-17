ab2caf4ef2282b34cd76b976f563aa068010b7ad
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 20:11:57 -0500

    TheNthIndx

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// calling Lang Integrated Query
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+	// Rtrn val of nth node of LL
+    {
+        if (n >= myLList.Count || myLList.Count == 0 || myLList == null || n < 0)
+		// if the node doesnt exist rtrn zero, start count at zero
+            return 0;
+        return myLList.ElementAt(n);
+		// elementAt returns element at a specfic indx in seqnc
+    }
+}
