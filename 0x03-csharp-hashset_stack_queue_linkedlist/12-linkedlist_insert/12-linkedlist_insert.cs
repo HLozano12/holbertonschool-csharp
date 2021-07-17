5e7500202d1ac2cec839afe41db07533052379a2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-16 20:44:00 -0500

    inster corrct pos

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+// link list
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+	// new node in corect position in ordered LL
+    {
+        int h = 0;
+		// val set at 0
+        
+        foreach (int node in myLList)
+		// Loop thru nodes in myList
+        {
+            if (h == n)
+			// if val is same as the int
+            {   
+                return node;
+            }
+            h++;
+        }
+        return 0;
+    }
+}
