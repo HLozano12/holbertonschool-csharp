88075b43a10edf652176ba89cc95023bf702272e
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-19 16:03:01 -0500

    Msg!

---
+++ 0x04-csharp-exceptions/4-throw_exception_msg/4-throw_exception_msg.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Except
+    {
+        public static void ThrowMsg(string msg)
+		// throw exception with a msg
+        {
+			throw new Exception(msg);
+        }
+    }
