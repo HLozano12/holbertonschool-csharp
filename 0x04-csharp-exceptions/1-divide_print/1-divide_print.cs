8c2ea5c545a4ccd079182ff18b2b11292bf8ff3a
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-19 13:18:59 -0500

    Divide zero?

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+    class Int
+    {
+        public static void divide(int a, int b)
+		// divide 2 ints and print result
+		// use try/catch/finally
+		// quotation should print in finally format
+        {
+            int result = 0;
+			try
+			{
+				result = a / b;
+				// devide the ints
+			}
+			catch (DivideByZeroException)
+			// if exception block will handle
+			// if int or decimal dividied by zero
+			{
+				Console.WriteLine("Cannot divide by zero");
+			}
+			finally
+			// executes whether try block terms norm or terms due to exception
+			{
+				Console.WriteLine("{0} / {1} = {2}", a, b, result);
+			}
+        }
+	}
