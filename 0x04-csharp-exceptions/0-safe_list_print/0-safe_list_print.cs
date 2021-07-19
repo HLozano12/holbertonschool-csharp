cd289a4b9504417aa66e9c3f57c70302989d5e78
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-19 13:01:14 -0500

    Try/Catch

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+		// print n elements of a list
+		// use try / catch
+		// int n is # of elements to print
+        {
+            int h = 0;
+
+			foreach (int element in myList)
+			try
+			// Holds the suspected code w possible exceptions
+			{
+				if (h < n)
+				{
+					Console.WriteLine(element);
+					h++;
+				}
+			}	
+			catch
+				// catch exception w/ exception handle
+			{
+				return h;	
+			}
+			return h;
+        }
+    }
