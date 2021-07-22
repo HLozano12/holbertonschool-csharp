d386c44100aa37ddc06125dc718154fb7e5f42ae
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-21 19:10:13 -0500

    assignHealth

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -5,12 +5,14 @@ namespace Enemies
     ///<summary>Zombie class</summary>
     class Zombie
     {
-        public int health;
 		///<summary> class health field</summary>
-        public Zombie()
+        public int health;
+
 		///<summary>Class public constructor</summary>
+        public Zombie()
+		
         {
-			
+			health = 0;
         }
     }
 }
