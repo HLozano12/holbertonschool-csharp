1f3f31ee8e5ee6f6f292a3041997667c19dfc89a
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-21 19:18:40 -0500

    HealthValid

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Rerpresents Zombie class</summary>
+    class Zombie
+    {
+		///<summary>Zombie health field</summary>
+        public int health;
+
+		///<summary>Zombie public constructor</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+    }
+}
