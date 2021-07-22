010328dfd0dfdbaaa8978627e89d50f0719bdedf
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-21 19:59:38 -0500

    EdZombie

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie class</summary>
+    class Zombie
+    {
+		///<summary>Class Private field</summary>
+        public int health;
+        private string name = "(No name)";
+        public Zombie()
+        {
+            health = 0;
+        }
+		///<summary>public constructor with a value</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+
+        public string Name
+        {
+            get {return (name);}
+            set {name = value;}
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
