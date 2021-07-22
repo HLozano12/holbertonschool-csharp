8720723644ab0969597d91d3abaa2370e2d3b9e8
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-21 19:03:41 -0500

    Health Zero

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+		///<summary> class health field</summary>
+        public Zombie()
+		///<summary>Class public constructor</summary>
+        {
+			
+        }
+    }
+}
