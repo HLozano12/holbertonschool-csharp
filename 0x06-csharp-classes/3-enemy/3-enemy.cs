2c572dcc722ae68322dc0222e78e95ab0f19525a
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-21 19:45:16 -0500

    ZombieDefZOmbie

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Rerpresents Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+		}
+		///<summary>Zombie public constructor with a value</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+
+		///<summary>Zombie public method returning health value</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
