87da151571eb5d19b1a653aad85a80ef36d4f090
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-25 13:43:48 -0700

    Test Sum

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyMath
+{
+	/// <summary> Operations is class given </summary>
+    public class Operations
+    {
+		/// <summary> Add function adds two ints to return sum </summary>
+		public static int Add(int a, int b)
+		{
+			int sum = (a + b);
+			return (sum);
+		}
+    }
+}
