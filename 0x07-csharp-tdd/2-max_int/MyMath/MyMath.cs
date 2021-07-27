78a247768e59c266f6d5b9f0301028da42f00d9f
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-26 20:45:55 -0700

    Max

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    ///<summary>Define functions in the Operations class</summary>
+    public class Operations
+    {
+        ///<summary>Max method returns max int in a given list</summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+            {
+                return 0;
+            }
+    
+           int maximum = nums[0];
+           foreach (int numbers in nums)
+           {
+               if (numbers > maximum)
+               {
+                   maximum = numbers;
+               }
+           }
+           return maximum;
+        }
+    }
+}
