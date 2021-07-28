d007bc764568d8dd2115705e05fb4e4d043fffc4
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-27 22:00:14 -0700

    JustMyType

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+/// <summary>Obj class given per proj</summary>
+class Obj
+{
+    /// <summary>Prints the names of the properties of an obj</summary>
+        public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        Console.WriteLine($"{t.Name} Properties:");
+        foreach (PropertyInfo i in t.GetProperties())
+        {
+            Console.WriteLine(i.Name);
+        }
+        Console.WriteLine($"{t.Name} Methods:");
+        foreach (MethodInfo x in t.GetMethods())
+        {
+            Console.WriteLine(x.Name);
+        }
+    }
+}
