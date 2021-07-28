1083e80d4709f749b88d6393ab2855d5974e6aaf
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-27 21:26:30 -0700

    Inheritance

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary> return true if the obj is an instance of or if the obj</summary>
+class Obj
+{
+    ///<summary>Returns true if an instance of class</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (obj is Array);
+    }
+}
