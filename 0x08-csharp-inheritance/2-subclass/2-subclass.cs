62dc58fbcaabe84bf044df84f6344b9977c065b8
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-27 21:45:40 -0700

    SubClassy

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+///<summary>Obj Class Given by proj</summary>
+class Obj
+{
+    ///<summary>IsOnlyASubclass returns true or false per  baseType.</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return (true);
+        }
+
+        else 
+        {
+            return (false);
+        }
+    }
+}
