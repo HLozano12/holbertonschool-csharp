d93bb39eefa3528719ada63f8f77b0ba93dba685
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-27 20:51:39 -0700

    TrueFalse

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+///<summary>Our given Class </summary>
+class Obj 
+{
+    ///<summary>IsOfTypeInt returns true or false</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+		///<summary> GetType gets the type of current instance</summary>
+        if (obj.GetType() == typeof(int))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
