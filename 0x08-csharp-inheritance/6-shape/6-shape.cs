ea0f46e44f616be2d5a45cea8e9fe2b51ddaf3f7
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-29 21:09:30 -0700

    Rectangle

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/// <summary> Our given Base Class </summary>
+class Shape
+{
+    /// <summary> Our area of the shape </summary>
+    public virtual int Area()
+    {
+    throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary> Rectangle class, WxH </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary> Obtains the width, positive ints </summary>
+    public int Width { 
+        get { return width; }
+
+        set {
+        if (value < 0) {throw new ArgumentException("Width must be greater than or equal to 0");}
+        width = value;}
+    }
+    /// <summary> Obtains Height </summary>
+    public int Height {
+        get { return height; }
+
+        set
+        {
+        if (value < 0) {throw new ArgumentException("Height must be greater than or equal to 0");}
+        height = value;}
+    }
+}
