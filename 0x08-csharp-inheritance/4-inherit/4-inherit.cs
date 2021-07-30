66bdb707aa21a4f0aecb401065cd674cb04b5088
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-29 20:38:44 -0700

    SubDerived Class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Empty class to be inherited from</summary>
+class Animal
+{
+
+}
+///<summary>Derived class to inherit Base class</summary>
+class Dog : Animal
+{
+	
+}
