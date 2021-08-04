902f9870e31b05e3162c46e3260e0f132431bb55
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-04 14:20:31 -0500

    Enqueue

---
+++ 0x01-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+///<summary> Queue class</summary>
+class Queue<T>
+{
+    public Node head = null;
+    public Node tail = null;
+    public int count;
+
+    ///<summary> Public class Node </summary>
+    public class Node 
+    {
+        private T value = default(T);
+        public Node next = null; 
+
+        public Node(T h)
+        {
+            value = h;
+        }
+    }
+
+    ///<summary> returns the type</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    ///<summary> Method that creates/adds new node to the end</summary>
+    public void Enqueue(T value)
+    {
+        Node Nnode = new Node(value);
+
+        if (head == null)
+        {
+            head = Nnode;
+        }
+        else if (tail == null)
+        {
+            tail = Nnode;
+        }
+        else
+        {
+            tail.next = Nnode;
+            tail = Nnode;
+        }
+
+        count++;
+    }
+
+    ///<summary> returns the count</summary>
+    public int Count()
+    {
+        return count;
+    }
+}
