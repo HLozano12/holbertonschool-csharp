ec3644703f8b396f3dcae8099c12c6d82b09fb3d
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-04 18:20:51 -0500

    Ab THinking

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary> abstract class  </summary>
+public abstract class Base
+{
+    /// <summary> Public Property </summary>
+    public string name;
+
+    /// <summary> ToString override </summary>
+    public override string ToString()
+    {
+        return String.Format("{0} is a {1}", name, this.GetType().Name);
+    }
+}
