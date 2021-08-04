8cbbc03d6d9fc648bb2a986ddf0b0034fbaaa2d4
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-04 18:48:17 -0500

    Interface

---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+///<summary> Base class</summary>
+abstract class Base
+{
+    ///<summary> public name</summary>
+    public string name { get; set; }
+
+    ///<summary>overriding ToString</summary>
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().ToString());
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Collect() {}
+    public void Break() {}
+    public void Interact() {}
+}
