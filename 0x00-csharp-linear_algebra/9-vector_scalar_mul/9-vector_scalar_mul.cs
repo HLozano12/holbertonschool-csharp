45bfcc8cdee9320e07659f481761234d66744b96
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 12:49:30 -0500

    Multiply

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+///<summary>Class for Vectors.</summary>
+class VectorMath
+{
+    ///<summary>Protype given</summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] result = new double[1] {-1};
+
+        if (vector.Length == 2)
+        {
+            result = new double[2] {(vector[0] * scalar), (vector[1] * scalar)};
+        }
+        else if (vector.Length == 3)
+        {
+            result = new double[3] {(vector[0] * scalar), (vector[1] * scalar), (vector[2] * scalar)};
+        }
+        return result;
+    }
+}
