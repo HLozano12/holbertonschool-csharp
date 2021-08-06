ca1424ced42420765e6210b7a05fe19f4a0802f2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 13:17:32 -0500

    Calc Dot

---
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+///<summary>Class of Vectors</summary>
+class VectorMath
+{
+    ///<summary>Given Prototype</summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double result = -1;
+        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length) 
+        {
+            result = 0;
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                result += vector1[i] * vector2[i];
+            }
+        }
+        return result;
+    }
+}
