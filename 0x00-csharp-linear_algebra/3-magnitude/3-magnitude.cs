b534f7c28d225fa3fa8a1dc96d118462bc457db0
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 12:07:02 -0500

    XML issues

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -8,17 +8,15 @@ class VectorMath
     public static double Magnitude(double[] vector)
     {
         double result;
-		///<summary> If 2D </summary>
         if (vector.Length == 2) {
             result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
             return Math.Round(result, 2);
             }
-		///<summary> If 3D </summary>	
         else if (vector.Length == 3) {
             result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
             return Math.Round(result, 2);
             }
-		///<summary> if not 2D or 3D </summary>	
+
         else
             return -1;
     }
