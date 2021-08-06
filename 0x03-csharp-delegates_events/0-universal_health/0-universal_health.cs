9583ecd29b6dbb0c16340d405485d26d30f79c9d
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 15:41:26 -0500

    PokeMon

---
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+///<summary>Public Class called Player</summary>
+class Player
+{
+    ///<summary> Properties </summary>
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+
+    ///<summary> constructor for the player stats</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary> prints health prototype</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+}
