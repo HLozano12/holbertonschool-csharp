28a8a585135739100bd4dbe86e351fe34f403721
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 12:39:24 -0500

    AddTwo

---
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Adds two vectors and returns resulting vector</summary>
+class VectorMath
+{
+    ///<summary> Prototype given </summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+
+        double[] ret = new double[1]{-1};
+        if (vector1.Length == 2 && vector2.Length == 2)
+            ret = new double[2] {(vector1[0] + vector2[0]), (vector1[1] + vector2[1])};
+        if (vector1.Length == 3 && vector2.Length == 3)
+            ret = new double[3] {(vector1[0] + vector2[0]), (vector1[1] + vector2[1]), (vector1[2] + vector2[2])};
+        
+        
+        return ret;
+    }
+}
