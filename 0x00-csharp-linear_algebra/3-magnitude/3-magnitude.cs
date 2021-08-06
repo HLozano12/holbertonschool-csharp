aeff624f054325bec0d75a1d7f32b5a662baf3cc
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 11:20:40 -0500

    2D or 3D

---
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary> Our Vector Class </summary>
+class VectorMath
+{
+	///<summary> Given Prototype </summary>
+    public static double Magnitude(double[] vector)
+    {
+        double result;
+		///<summary> If 2D </summary>
+        if (vector.Length == 2) {
+            result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            return Math.Round(result, 2);
+            }
+		///<summary> If 3D </summary>	
+        else if (vector.Length == 3) {
+            result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            return Math.Round(result, 2);
+            }
+		///<summary> if not 2D or 3D </sumarry>	
+        else
+            return -1;
+    }
+}
