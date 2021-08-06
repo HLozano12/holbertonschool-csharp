ac10ec1134896dd0b167a221ec0c32c440e08912
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-08-06 14:31:25 -0500

    Can't Multi

---
+++ 0x00-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+///<summary> 
+class MatrixMath
+{
+    ///<summary>Prototype given for multipling two matrices </summary>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] NoMul = new double[,]{{-1}};
+        double[,] ProdMatrix;
+        int row = matrix1.GetLength(0);
+        int col = matrix1.GetLength(1);
+        int row2 = matrix2.GetLength(0);
+        int col2 = matrix2.GetLength(1);
+    
+    if (col != row2)
+    {
+        return NoMul;
+    }
+
+    ProdMatrix = new double [row, col2];
+    for (int h = 0; h < row; h++)
+    {
+        for (int k = 0; k < col2; k++)
+        {
+            double temp = 0;
+
+            for (int l = 0; l < col; l++)
+            {
+                temp += matrix1[h, l] * matrix2[l, k];
+            }
+            ProdMatrix[i,j] = temp;
+        }
+    }
+    return ProdMatrix;
+    }
+
+}
